804e078ef58a7420ef40206291617d114f5be3c8
Ramón Roche <mrpollo@gmail.com>
2014-05-12 10:14:16 -0700

    update first class international resctrictions

--- app/models/spree/calculator/shipping/usps/first_class_mail_international.rb
+++ app/models/spree/calculator/shipping/usps/first_class_mail_international.rb
@@ -2,7 +2,20 @@ module Spree
   module Calculator::Shipping
     module Usps
       class FirstClassMailInternational < Spree::Calculator::Shipping::Usps::Base
-        WEIGHT_LIMITS = { "US" => 3.5 }
+        AVAILABLE_COUNTRIES = [
+          "AC", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AR", "AT", "AU", "AW", "AZ", "BA", "BB", "BD",
+          "BE", "BF", "BG", "BH", "BI", "BJ", "BM", "BN", "BO", "BR", "BS", "BT", "BW", "BY", "BZ", "CA", "CD", "CF",
+          "CG", "CH", "CI", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ",
+          "EC", "EE", "EG", "ER", "ES", "ET", "FI", "FJ", "FK", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GH", "GI",
+          "GL", "GM", "GN", "GP", "GQ", "GR", "GT", "GW", "GY", "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IN",
+          "IQ", "IR", "IS", "IT", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KR", "KW", "KY", "KZ", "LA",
+          "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MD", "ME", "MG", "MK", "ML", "MM", "MN",
+          "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL",
+          "NO", "NP", "NR", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PT", "PY", "QA", "RE",
+          "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SK", "SL", "SM", "SN", "SR", "ST",
+          "SV", "SY", "SZ", "TC", "TD", "TG", "TH", "TJ", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG",
+          "UY", "UZ", "VA", "VC", "VE", "VG", "VN", "VU", "WF", "WS", "YE", "ZA", "ZM", "ZW"
+        ]
 
         def self.service_code
           13 #First-Class Mail® International Letter
@@ -12,16 +25,14 @@ module Spree
           "USPS First-Class Mail International Letter"
         end
 
-
         protected
         def max_weight_for_country(country)
           # if weight in ounces > 3.5, then First Class Mail International is not available for the order
           # https://www.usps.com/ship/first-class-international.htm
-          return WEIGHT_LIMITS[country.iso]
+          return 3.5 if AVAILABLE_COUNTRIES.include?(country.iso)
+          nil
         end
       end
     end
   end
 end
-
-
--- app/models/spree/calculator/shipping/usps/first_class_mail_international_large_envelope.rb
+++ app/models/spree/calculator/shipping/usps/first_class_mail_international_large_envelope.rb
@@ -2,7 +2,20 @@ module Spree
   module Calculator::Shipping
     module Usps
       class FirstClassMailInternationalLargeEnvelope < Spree::Calculator::Shipping::Usps::Base
-        WEIGHT_LIMITS = { "US" => 64 }
+        AVAILABLE_COUNTRIES = [
+          "AC", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AR", "AT", "AU", "AW", "AZ", "BA", "BB", "BD",
+          "BE", "BF", "BG", "BH", "BI", "BJ", "BM", "BN", "BO", "BR", "BS", "BT", "BW", "BY", "BZ", "CA", "CD", "CF",
+          "CG", "CH", "CI", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ",
+          "EC", "EE", "EG", "ER", "ES", "ET", "FI", "FJ", "FK", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GH", "GI",
+          "GL", "GM", "GN", "GP", "GQ", "GR", "GT", "GW", "GY", "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IN",
+          "IQ", "IR", "IS", "IT", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KR", "KW", "KY", "KZ", "LA",
+          "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MD", "ME", "MG", "MK", "ML", "MM", "MN",
+          "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL",
+          "NO", "NP", "NR", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PT", "PY", "QA", "RE",
+          "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SK", "SL", "SM", "SN", "SR", "ST",
+          "SV", "SY", "SZ", "TC", "TD", "TG", "TH", "TJ", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG",
+          "UY", "UZ", "VA", "VC", "VE", "VG", "VN", "VU", "WF", "WS", "YE", "ZA", "ZM", "ZW"
+        ]
 
         def self.service_code
           14 #First-Class Mail® International Large Envelope
@@ -16,12 +29,11 @@ module Spree
         # weight limit in ounces or zero (if there is no limit)
         def max_weight_for_country(country)
           # if weight in ounces > 64, then First Class Mail International Large Envelope is not available for the order
-          # https://www.usps.com/ship/first-class-package-international-service.htm?
-          return WEIGHT_LIMITS[country.iso]
+          # https://www.usps.com/ship/first-class-international.htm?
+          return 64 if AVAILABLE_COUNTRIES.include?(country.iso) # 4lbs
+          nil # ex. North Korea, Somalia, etc.
         end
       end
     end
   end
 end
-
-
